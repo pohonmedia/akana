df87f970061f42b8e957e49803157222d6b60783
IQBALAQABA <iqbaldesignui@gmail.com>
2019-04-06 18:01:21 +0700

    init commit workspace

---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+const { src, dest, watch, parallel } = require('gulp');
+const notify = require('gulp-notify');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const plumber = require('gulp-plumber');
+const rename = require('gulp-rename');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+//dir
+var cssDir = './assets/css/';
+var jsDir = './assets/js/';
+var sassDir = './assets/sass/*.scss';
+var imgDir = './assets/img/';
+
+//make structural folder
+function folder() {
+    return src('*.*', { read: false })
+        .pipe(dest('./sandbox/_test'))
+        .pipe(dest('./assets'))
+        .pipe(dest('./assets/sass'))
+        .pipe(dest('./assets/css'))
+        .pipe(dest('./assets/js'))
+        .pipe(dest('./assets/img'))
+        .pipe(notify({
+            message: 'Folder wis dadi'
+        }));
+}
+
+// manage js asset
+function jsAsset() {
+    return src([])
+        .pipe(plumber())
+        .pipe(concat('plugin.min.js'))
+        .pipe(uglify())
+        .pipe(dest(jsDir))
+        .pipe(notify({
+            message: 'File <%= file.relative %> wis dadi'
+        }));
+}
+
+// manage css asset
+function cssAsset() {
+    return src([])
+        .pipe(plumber())
+        .pipe(concat('plugin.min.css'))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(dest(cssDir))
+        .pipe(notify({
+            message: 'File <%= file.relative %> wis dadi'
+        }));
+}
+
+//sass-dev
+function sassdev() {
+    return src(sassDir)
+        .pipe(plumber())
+        .pipe(sass({
+            errorLogToConsole: true
+        }))
+        .on('error', console.error.bind(console))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(dest(cssDir))
+        .pipe(browserSync.stream());
+}
+
+//reload
+function reload(done) {
+    browserSync.reload();
+    done();
+}
+
+//watching
+function dev() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+        port: 8080
+    });
+    watch('./assets/sass/*.scss', sassdev);
+    watch('./', reload);
+    // watch('./public/assets/js/*.js', jsmin);
+}
+
+exports.updateAsset = parallel(cssAsset, jsAsset);
+exports.updateCss = cssAsset;
+exports.updateJs = jsAsset;
+exports.folder = folder;
+exports.default = dev;
